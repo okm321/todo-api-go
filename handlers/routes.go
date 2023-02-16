c4efc6800cd2d6fdb80d5d6ff6a53482bcfe1718
okamon <naofumi.okamoto@casting-one.jp>
2023-02-16 20:07:10 +0900

    cors対策

--- handlers/routes.go
+++ handlers/routes.go
@@ -6,6 +6,17 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+func handleOption(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.Header().Add("Access-Control-Allow-Origin", "http://localhost:8080")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Add("Access-Control-Allow-Headers", "Origin")
+	w.Header().Add("Access-Control-Allow-Headers", "X-Requested-With")
+	w.Header().Add("Access-Control-Allow-Headers", "Accept")
+	w.Header().Add("Access-Control-Allow-Headers", "Accept-Language")
+	w.Header().Set("Content-Type", "application/json")
+}
+
 func (app *Application) routes() *httprouter.Router {
 	router := httprouter.New()
 
@@ -13,6 +24,7 @@ func (app *Application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPost, "/api/todos", app.editTodo)
 	router.HandlerFunc(http.MethodGet, "/api/todos/:id", app.getOneTodo)
 	router.HandlerFunc(http.MethodDelete, "/api/todos/:id", app.deleteTodo)
+	router.OPTIONS("/*path", handleOption)
 
 	return router
 }
