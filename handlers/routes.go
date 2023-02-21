ce6459fff945650db7390c5995a30ecf23f3a1ee
okamon <naofumi.okamoto@casting-one.jp>
2023-02-21 10:14:10 +0900

    cors修正

--- handlers/routes.go
+++ handlers/routes.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handleOption(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	w.Header().Add("Access-Control-Allow-Origin", "https://codesandbox.io/")
+	w.Header().Add("Access-Control-Allow-Origin", "https://e7txy0.csb.app")
 	w.Header().Set("Access-Control-Allow-Methods", "*")
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Add("Access-Control-Allow-Headers", "Origin")
