f60b6114f146e887567bcec65df76f163d45009c
okamon <naofumi.okamoto@casting-one.jp>
2023-03-13 15:36:52 +0900

    不要な記述削除

--- handlers/routes.go
+++ handlers/routes.go
@@ -6,20 +6,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func handleOption(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	w.Header().Add("Access-Control-Allow-Origin", "https://e7txy0.csb.app")
-	w.Header().Set("Access-Control-Allow-Methods", "*")
-	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Add("Access-Control-Allow-Headers", "Origin")
-	w.Header().Add("Access-Control-Allow-Headers", "X-Requested-With")
-	w.Header().Add("Access-Control-Allow-Headers", "Accept")
-	w.Header().Add("Access-Control-Allow-Headers", "Accept-Language")
-	w.Header().Set("Content-Type", "application/json")
-}
-
 func (app *Application) routes() *httprouter.Router {
 	router := httprouter.New()
-	router.OPTIONS("/*path", handleOption)
 	router.HandlerFunc(http.MethodGet, "/api/todos", app.getAllTodos)
 	router.HandlerFunc(http.MethodPost, "/api/todos", app.editTodo)
 	router.HandlerFunc(http.MethodGet, "/api/todos/:id", app.getOneTodo)
